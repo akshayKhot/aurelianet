46b85782eda129d7505f402f8cfec6897af5a5e2
Akshay Khot <akshay.7277@gmail.com>
2018-01-24 15:39:58 -0800

    retrieve data from db and use it in view

---
+++ blog/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog.Controllers
+{
+    public class AboutController : Controller
+    {
+        private BloggingContext _context;
+        public AboutController(BloggingContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Home()
+        {
+            var author = _context.Authors.Single(auth => auth.ID == 1);
+            return View("About", author);
+        }
+        
+    }
+}
