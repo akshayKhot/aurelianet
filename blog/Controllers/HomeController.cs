d9c431bfb097f5beed6c63e0e096810da69bf106
Akshay Khot <akshay.7277@gmail.com>
2018-01-24 16:10:34 -0800

    loop over posts in View

--- blog/Controllers/HomeController.cs
+++ blog/Controllers/HomeController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using blog.Data;
 using Microsoft.AspNetCore.Mvc;
 using blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace blog.Controllers
 {
     public class HomeController : Controller
     {
+        private BloggingContext _context { get; set; }
+
+        public HomeController(BloggingContext context)
+        {
+            _context = context;
+        }
         public IActionResult Home()
         {
+            var posts = _context.Posts.ToList();
+            ViewData["Posts"] = posts;
             return View();
         }
     }
